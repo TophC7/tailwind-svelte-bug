823f587045710b7dd5d466ff30910425a595debd
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-12-01 14:37:18 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<a href="/about">go to about</a>
+
+<style>
+	a {
+		/* This class wont work here */
+		/* @apply test; */
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<div id="about">TEST</div>
+
+<style>
+	#about {
+		/* but it works here ?? */
+		@apply test;
+	}
+</style>
